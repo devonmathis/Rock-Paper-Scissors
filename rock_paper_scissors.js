252444931bbe4fa783786adfd6fc32a4cfe30302
devonmathis <d3689mathis@gmail.com>
2024-01-17 10:19:02 -0500

    Updated best of 5 functionality

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,37 +1,13 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = '';
+let roundDecision;
 
-let playerSelection = prompt("rock, paper, or scissors?");
 //let computerSelection = getComputerChoice();
 
-function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-    if (computerSelection === playerSelection){
-        roundWinner = 'tie';
-    }
-    if ((computerSelection === 'rock' && playerSelection === 'scissors') ||
-        (computerSelection === 'scissors' && playerSelection === 'paper') ||
-        (computerSelection === 'paper' && playerSelection === 'rock')) {
-        computerScore++;
-        roundWinner = 'computer';
-       }
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'rock')) {
-        playerScore++;
-        roundWinner = 'player';
-       }
-        //console.log("You chose " + playerSelection);
-        //console.log("The computer chose " + computerSelection);
-        //console.log(roundWinner);
-        //return roundWinner;
-}
-
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3);
-    //console.log(randomNumber);
-    switch(randomNumber){
+    let choices = Math.floor(Math.random() * 3);
+    switch(choices){
         case 0:
             return 'rock';
         case 1:
@@ -42,25 +18,55 @@ function getComputerChoice(){
     //console.log(getComputerChoice);
 }
 
+let playerSelection = prompt("rock, paper, or scissors?");
+//let computerSelection = getComputerChoice();
+
+function playRound(playerSelection) {
+    //let playerSelection = prompt("rock, paper, or scissors?");
+    let computerSelection = getComputerChoice();
+    if (computerSelection === playerSelection){
+        roundWinner = 'tie';
+        roundDecision = "You both chose " + playerSelection + ". It's a tie! Try again.";
+    }
+    if ((computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')) {
+        computerScore++;
+        roundWinner = 'computer';
+        roundDecision = "You lose... " + playerSelection + " loses to " + computerSelection + ". Choose again!";
+    }
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')) {
+        playerScore++;
+        roundWinner = 'player';
+        roundDecision = "You win! " + playerSelection + " beats " + computerSelection + "! Go again?";
+    }
+        //console.log("You chose " + playerSelection);
+        //console.log("The computer chose " + computerSelection);
+        //console.log(roundWinner);
+        return roundWinner, roundDecision;
+}
+
 function game(){
     playRound(playerSelection);
     if (roundWinner === 'tie'){
-        console.log("You both chose " + playerSelection + ". It's a tie! Try again.");
+        console.log(roundDecision);
     }
     else if (roundWinner === 'computer'){
-        console.log("You lose... " + playerSelection + " loses to " + getComputerChoice() + ". Choose again!");
+        console.log(roundDecision);
     }
     else if (roundWinner === 'player'){
-        console.log("You win! " + playerSelection + " beats " + getComputerChoice() + "! Double down?");
+        console.log(roundDecision);
     }
-    console.log("You chose " + playerSelection);
-    console.log("The computer chose " + getComputerChoice());
+    //console.log("You chose " + playerSelection);
+    //console.log("The computer chose " + `${playRound.computerSelection}`);
     gameOver();
 }
 
 function gameOver(){
     if (computerScore === 5){
-        console.log("You lose. Game over.");
+        console.log("You lost it all. Game over.");
     }
     else if (playerScore === 5){
         console.log("You've won! Rematch?");
